1251e51aedadb220e219137cdeef9a3a79a0eb5a
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-01-08 14:00:49 +0100

    update camera
    FixedUpdate() pour drag camera
    dragCamera fonctionel
    Commenaire du code à finir

--- Rendu/Alpha/Assets/Scripts/CameraFollowMouse.cs
+++ Rendu/Alpha/Assets/Scripts/CameraFollowMouse.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class CameraFollowMouse : MonoBehaviour 
 {
-
+    //Transform de la caméra
     [SerializeField]
     private Transform _transform;
 
+    //Début de la zone permettant le déplacement souris
     [SerializeField]
     public float activeZoneBegin = 0.95f;
-
+    
+    //Fin de la zone permettant le déplacement souris
     [SerializeField]
     public float activeZoneEnd = 0.99f;
 
+    //Vitesse de déplacement de la caméra
     [SerializeField]
     public float moveSpeed = 1f;
 
@@ -28,7 +31,7 @@ public class CameraFollowMouse : MonoBehaviour
         Vector2 mousePosition = Vector3.zero;
         Vector3 cameraPosition = _transform.position;
 
-        mousePosition.Set(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+        mousePosition.Set(Input.mousePosition.x/Screen.width, Input.mousePosition.y/Screen.height);
 
         if ( (mousePosition.x <= 1 - activeZoneBegin) && (mousePosition.x >= 1 - activeZoneEnd) )
         {
--- Rendu/Alpha/Assets/Scripts/CameraZoom.cs
+++ Rendu/Alpha/Assets/Scripts/CameraZoom.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class CameraZoom : MonoBehaviour 
 {
-
+    //Transform de la caméra
     [SerializeField]
     private Transform _transform;
 
+    //Vitesse de zoom/dézoom
     [SerializeField]
     private float scrollSpeed = 5f;
 
+    //Hauteur caméra min (zoom max)
     [SerializeField]
     public float zoomInLimit = 5f;
 
+    //Hauteur de caméra max (dézoom max)
     [SerializeField]
     public float zoomOutLimit = 10f;
 
@@ -24,12 +27,18 @@ public class CameraZoom : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () 
-    {     
+    {   
+        //On déplare la variable qui contiendra le mouvement caméra de la molette souris
+        //et on récupère la position de la camera
         float scrollMove;
         Vector3 cameraPosition = _transform.position;
-          
+        
+        //Affecte la valeur emise par l'utilisation de la molette (valeur allant de -1 à 1) 
         scrollMove = Input.GetAxis("Mouse ScrollWheel")*scrollSpeed;
 
+        //Application du mouvement sur l'axe y de la caméra
+        //Scolling si on est entre les limites de hauteur autorisé
+        //Si on a dépassé une limite de hauteur on se remet à la limite
         if ((cameraPosition.y - scrollMove >= zoomInLimit) && (cameraPosition.y - scrollMove <= zoomOutLimit))
         {
             cameraPosition.y -= scrollMove;
@@ -43,6 +52,7 @@ public class CameraZoom : MonoBehaviour
             cameraPosition.y = zoomOutLimit;
         }
 
+        //Application du déplacement sur le transform de la caméra
         _transform.position = cameraPosition;
 	}
 }
