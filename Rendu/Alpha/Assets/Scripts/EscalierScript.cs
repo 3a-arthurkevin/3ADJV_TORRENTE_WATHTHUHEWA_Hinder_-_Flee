0a1e1fd9c6cafa14b94f08ba355cda2b520e0496
Arthur TORRENTE <torrente.arthur@gmail.com>
2014-01-29 01:01:09 +0100

    Création script Escalier

---
+++ Rendu/Alpha/Assets/Scripts/EscalierScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscalierScript : MonoBehaviour {
+
+    private TextAsset m_image;
+    private Texture2D m_newCursor;
+
+    void Start ()
+    {
+        m_newCursor = new Texture2D(32, 32);
+	    m_newCursor.LoadImage(m_image.bytes);
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
