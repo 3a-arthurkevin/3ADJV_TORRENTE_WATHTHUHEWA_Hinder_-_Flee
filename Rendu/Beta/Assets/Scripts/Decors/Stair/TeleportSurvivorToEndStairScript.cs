bfc4163196a6141f62fe4109d24452ebce5ed895
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-03-03 21:08:08 +0100

    debut limiter camera
    fonction recherche GameObject "Floor"
    Modification du nom du gameObject FLoor0 en Floor0 (L -> l)

--- Rendu/Beta/Assets/Scripts/Decors/Stair/TeleportSurvivorToEndStairScript.cs
+++ Rendu/Beta/Assets/Scripts/Decors/Stair/TeleportSurvivorToEndStairScript.cs
@@ -24,11 +24,78 @@ public class TeleportSurvivorToEndStairScript : MonoBehaviour {
         m_hasClicked = false;
     }
 
+    //Fonction booléen avec pointeur en sortie
+    //Recherche (parmi les GameObjects) l'etage sur lequelle se trouve l'escalier de sortie (utiliser par le suvivant)
+    bool findCurrentFloor(out GameObject floor)
+    {
+        bool findFloor = false;
+        int nameLength = 0;
+
+        //Logiquement l'escalier de sortie est forcement enfant de quelque chose (au moins Level)
+        floor = m_stairOut.transform.parent.gameObject;
+        //Limite de tour de boucle pour trouver l'etage courrant
+        int limit = 0; 
+
+        while (limit<10)
+        {
+            Debug.LogError(floor.name);
+            nameLength = floor.name.Length;
+            if (nameLength >= 5)
+            {
+                if (Equals(floor.name.Substring(0, 5), "Floor"))
+                {
+                    return true;
+                }
+            }
+            if (floor.transform.parent)
+            {
+                floor = floor.transform.parent.gameObject;
+            }
+            else
+            {
+                floor = null;
+                Debug.LogError("404 No floor found");
+                return false;
+            }
+            limit++;
+        }
+        floor = null;
+        return findFloor;
+    }
+
     void OnTriggerStay(Collider survivor)
     {
         if (m_hasClicked)
-        {//TP survivor
+        {
+            //TP survivor lors qu'il prend l'escalier
+            //Mise à jour des limites de la camera du joueur
             survivor.transform.position = m_stairOut.position;
+            
+            GameObject floor = null;
+            GameObject camBorder = null;
+
+            if (findCurrentFloor(out floor))
+            {
+                camBorder = floor.transform.FindChild("camBorder").gameObject;
+
+                /*
+                try
+                {
+                    récupérer la cam du joueur pour appeler la fonction setLimite
+                
+                 * Soit jouer avec le nom de la camera lors de son instanciation en lancement de partie
+                 * Soit RPC pour mettre à jour limiter camera dans playerDataBaseScript 
+                }
+                catch (UnityException e)
+                {
+                    return;
+                }
+                */
+            }
+            else
+            {
+                Debug.LogError("Error 404 : No floor found");
+            }
         }
     }
 }
