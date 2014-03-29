cf22e582c5cc47fc1810166ed3773f9e1e63e3f8
Arthur TORRENTE <torrente.arthur@gmail.com>
2014-03-29 22:44:18 +0100

    suppr des message de debug

--- Rendu/Beta/Assets/Scripts/Zombies/MoveManagerZombieScript.cs
+++ Rendu/Beta/Assets/Scripts/Zombies/MoveManagerZombieScript.cs
@@ -42,14 +42,10 @@ public class MoveManagerZombieScript : MonoBehaviour {
     {
 
         if (m_follow)
-        {
-            Debug.Log("Follow");
             genPath(m_data.Position.position, m_survivor.position);
-        }
 
         if (m_data.Path == null)
         {
-            Debug.Log("Path null");
             if (Network.isServer)
                 m_networkView.RPC("setTarget", RPCMode.All, ConfigLevelManager.getRandomMoveZombie(m_data.IsInFloor));
             
@@ -61,12 +57,8 @@ public class MoveManagerZombieScript : MonoBehaviour {
 
         if (direction.sqrMagnitude < m_minDistance)
         {
-            Debug.Log("Chemin trop court");
-
             if ((m_data.NumCorner + 1) >= m_data.Path.corners.Length)
-            {
-                Debug.Log("Pas plus de corner");
-                
+            {   
                 if (m_follow)
                     return;
 
@@ -78,7 +70,6 @@ public class MoveManagerZombieScript : MonoBehaviour {
             }
             else
             {
-                Debug.Log("Change direction");
                 Vector3 look = m_data.Path.corners[++m_data.NumCorner];
                 look.y = m_data.Position.position.y;
                 m_data.Position.LookAt(look);
@@ -86,85 +77,6 @@ public class MoveManagerZombieScript : MonoBehaviour {
         }
 
         m_data.Position.position += direction.normalized * m_data.Speed * Time.deltaTime;
-
-        /*
-        if (Network.isClient)
-        {//Client side
-            Vector3 direction = m_target - m_data.Position.position;
-
-            if (direction.sqrMagnitude >= m_minDistance)
-                m_data.Position.position += direction.normalized * m_data.Speed * Time.deltaTime;
-        }
-        else
-        {//Server side
-            Vector3 direction = Vector3.zero;
-
-            if (m_follow)
-            {
-                m_data.Path = MoveUtilsScript.getCalcPath(m_data.Position.position, m_survivor.position);
-
-                if (m_data.Path == null)
-                {
-                    m_data.NumCorner = 0;
-                    m_target = Vector3.zero;
-                }
-                else
-                {
-                    m_data.NumCorner = 1;
-                    direction = m_data.Path.corners[1] - m_data.Position.position;
-                    m_data.Position.LookAt(m_data.Path.corners[1]);
-                    m_target = m_data.Path.corners[1];
-                }
-            }
-            else
-            {
-                if (m_data.Path == null)
-                    getRandomPath();
-                
-                else
-                {
-                    direction = m_data.Path.corners[m_data.NumCorner] - m_data.Position.position;
-
-                    if (direction.sqrMagnitude < m_minDistance)
-                    {
-                        if ((m_data.NumCorner + 1) >= m_data.Path.corners.Length)
-                            m_data.Path = null;
-
-                        else
-                        {
-                            Vector3 look = m_data.Path.corners[++m_data.NumCorner];
-                            look.y = m_data.Position.position.y;
-                            m_data.Position.LookAt(look);
-                        }
-
-                        direction = Vector3.zero;
-                    }
-                }
-            }
-
-            direction.y = 0;
-            m_data.Position.position += direction.normalized * m_data.Speed * Time.deltaTime;
-        }*/
-    }
-
-    private void getRandomPath()
-    {
-        /*
-        if (Network.isServer)
-        {
-            Vector3 target = ConfigLevelManager.getRandomMoveZombie(m_atIsFloor);
-            m_data.Path = MoveUtilsScript.getCalcPath(m_data.Position.position, target);
-
-            if (m_data.Path != null)
-            {
-                m_data.NumCorner = 1;
-                m_target = m_data.Path.corners[1];
-                
-                Vector3 look = m_data.Path.corners[1];
-                look.y = m_data.Position.position.y;
-                m_data.Position.LookAt(look);
-            }
-        }*/
     }
 
     public void Follow(Transform target)
@@ -188,9 +100,6 @@ public class MoveManagerZombieScript : MonoBehaviour {
 
     public void genPath(Vector3 origin, Vector3 target)
     {
-
-        Debug.Log(target.ToString("F6"));
-
         m_data.Path = MoveUtilsScript.getCalcPath(origin, target);
 
         if (m_data.Path != null)
