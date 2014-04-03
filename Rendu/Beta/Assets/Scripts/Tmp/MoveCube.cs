b56246a13db178a71daac8ffc4b586f215ea6657
Arthur TORRENTE <torrente.arthur@gmail.com>
2014-04-03 20:51:23 +0200

    up

---
+++ Rendu/Beta/Assets/Scripts/Tmp/MoveCube.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+	
+    void FixedUpdate ()
+    {
+        transform.position += Vector3.right * 2 * Time.deltaTime;
+	}
+}
