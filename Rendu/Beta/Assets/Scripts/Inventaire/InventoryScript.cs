ce93a8a73190215d9a2b697f818b12db380ba459
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-04-15 23:52:51 +0200

    Object corner + Inventory + Slot

---
+++ Rendu/Beta/Assets/Scripts/Inventaire/InventoryScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryScript : MonoBehaviour
+{
+    [SerializeField]
+    int m_quantityMaxForOneItem = 10;
+
+    [SerializeField]
+    int m_nbSlotInventory = 10;
+    
+    List<Slot> m_inventory;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_inventory = new List<Slot>();
+
+        for (int i = 0; i < m_nbSlotInventory; i++)
+        {
+            m_inventory.Add(new Slot());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public Slot getItem(int index)
+    {
+        return m_inventory[index];
+    }
+
+    public void AddItem(int idItemToAdd/*, int quantityToAdd*/)
+    {
+        foreach (Slot slotInventory in m_inventory)
+        {
+            if (slotInventory.id == idItemToAdd)
+            {
+                slotInventory.setQuantity(1/*quantityToAdd*/, m_quantityMaxForOneItem);
+            }
+        }
+    }
+
+    public void throwItem(int idItemToThrow)
+    {
+        m_inventory[idItemToThrow].resetSlot(); 
+    }
+}
---
+++ Rendu/Beta/Assets/Scripts/Inventaire/Slot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slot 
+{
+    private int m_idItem;
+    private int m_quantity;
+
+    public Slot()
+    {
+        this.m_idItem = -1;
+        this.m_quantity = 0;
+    }
+
+    public Slot(int pId, int pQuantite)
+    {
+        this.m_idItem = pId;
+        this.m_quantity = pQuantite;
+    }
+
+    public int id
+    {
+        get { return m_idItem; }
+        set { m_idItem = value; }
+    }
+
+    public void setQuantity(int value, int quantityMax)
+    {
+        this.m_quantity += value;
+
+        if (this.m_quantity > quantityMax)
+        {
+            this.m_quantity = quantityMax;
+        }
+    }
+
+    public int getQuantity()
+    {
+        return this.m_quantity;
+    }
+
+    public void resetSlot()
+    {
+        this.m_idItem = -1;
+        this.m_quantity = 0;
+    }
+}
