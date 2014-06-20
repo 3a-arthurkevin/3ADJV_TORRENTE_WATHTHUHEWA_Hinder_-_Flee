85c082e115397eae4ed6102a67e71f39d9b86d37
3a-arthurkevin <torrente.arthur@gmail.com>
2014-06-21 01:01:09 +0200

    Ajout des SpawnPoint

---
+++ Rendu/RC/Assets/Scripts/Manager/SpawnPointManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointManager : MonoBehaviour {
+    [SerializeField]
+    public int m_floor;
+    
+    [SerializeField]
+    public Transform[] m_spawnSurvivor;
+
+    [SerializeField]
+    public Transform[] m_spawnZombie;
+}
---
+++ Rendu/RC/Assets/Scripts/Utils/SpawnPointScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointScript : MonoBehaviour {
+    public enum SpawnType
+    {
+        Survivor,
+        Zombie
+    }
+    [SerializeField]
+    public SpawnType spawType;
+
+    [SerializeField]
+    private Transform m_transform;
+
+    void OnDrawGizmos()
+    {
+        if (spawType == SpawnType.Zombie)
+            Gizmos.color = Color.red;
+        
+        else if (spawType == SpawnType.Survivor)
+            Gizmos.color = Color.blue;
+
+        Gizmos.DrawWireSphere(m_transform.position, 2);
+    }
+}
